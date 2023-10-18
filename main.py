ca6180de679d6b63507d0010f92d3c2fa71ced37
intrafellow <reol04@ya.ru>
2023-10-18 16:02:20 +0300

    Added compatibility with Python 3.5

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ cur = conn.cursor()
 workbook = openpyxl.load_workbook('mcc-mnc.xlsx')
 sheet = workbook.active
 
-for row in sheet.iter_rows(min_row=2, values_only=True):
-    mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands = row
+for row in sheet.iter_rows(min_row=2):
+    mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands = (cell.value for cell in row)
     cur.execute("INSERT INTO mcc_mnc_storage (MCC, MNC, PLMN, Region, Country, ISO, Operator, Brand, TADIG, Bands) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                 (mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands))
 
