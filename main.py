41764a6ee86a74fa3f3884fee96a1060d9145cb1
intrafellow <reol04@ya.ru>
2023-10-22 17:37:57 +0300

    Updated code for database interaction

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import json
 import psycopg2
 import openpyxl
+import os
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+
+def file_exists(file_path):
+    return os.path.exists(file_path) and os.access(file_path, os.R_OK)
+
+
+config_file_path = 'config.json'
+if file_exists(config_file_path):
+    with open(config_file_path, 'r') as config_file:
+        config = json.load(config_file)
+else:
+    raise FileNotFoundError("Config file not found or not readable.")
 
 conn = psycopg2.connect(
     database=config["database"],
@@ -15,14 +25,29 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-workbook = openpyxl.load_workbook('mcc-mnc.xlsx')
-sheet = workbook.active
+xlsx_file_path = 'mcc-mnc.xlsx'
+if file_exists(xlsx_file_path):
+    workbook = openpyxl.load_workbook(xlsx_file_path)
+    sheet = workbook.active
 
-for row in sheet.iter_rows(min_row=2):
-    mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands = (cell.value for cell in row)
-    cur.execute("INSERT INTO mcc_mnc_storage (MCC, MNC, PLMN, Region, Country, ISO, Operator, Brand, TADIG, Bands) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    for row in sheet.iter_rows(min_row=2):
+        mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands = (cell.value for cell in row)
+
+        cur.execute("SELECT MCC, MNC FROM mcc_mnc_storage WHERE MCC = %s AND MNC = %s", (mcc, mnc))
+        existing_record = cur.fetchone()
+
+        if existing_record:
+            cur.execute(
+                "UPDATE mcc_mnc_storage SET PLMN = %s, Region = %s, Country = %s, ISO = %s, Operator = %s, Brand = %s, TADIG = %s, Bands = %s WHERE MCC = %s AND MNC = %s",
+                (plmn, region, country, iso, operator, brand, tadig, bands, mcc, mnc))
+        else:
+            cur.execute(
+                "INSERT INTO mcc_mnc_storage (MCC, MNC, PLMN, Region, Country, ISO, Operator, Brand, TADIG, Bands) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                 (mcc, mnc, plmn, region, country, iso, operator, brand, tadig, bands))
 
-conn.commit()
+    conn.commit()
+else:
+    raise FileNotFoundError("XLSX file not found or not readable.")
+
 cur.close()
 conn.close()
